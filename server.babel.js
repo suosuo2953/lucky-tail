35cdaec711406a9da467666af7e9251a88e52c83
王瑞存 wangruicun () <wangruicun@tuniu-inc.com>
2017-04-10 14:25:29 +0800

    update

--- server.babel.js
+++ server.babel.js
@@ -1,6 +1,1 @@
-import express from 'express';
 
-const app = express();
-
-app.use('/', express.static('public'));
-app.listen(process.env.PORT || 8080);
--- server.js
+++ server.js
@@ -1,2 +1,10 @@
-require('babel-register');
-require('./server.babel');
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
+
+http.createServer(app).listen(8080, () => {});
